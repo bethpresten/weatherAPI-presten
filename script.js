cb1580bbbaa61e145b1e6f82976e11b7ad78686e
Beth Presten <bethpresten@gmail.com>
2020-10-11 19:58:17 -0400

    removed index.html, got divs lined up side by side, began to add jquery to js page

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ console.log("This is connected!")
 $(document).ready(function () {
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid=e2ce71516635d8291df50c096e9d84fd"
 
-
+    $("search").on("click", function (event) {
+        var city = $("search-city").val();
+    })
 
 
 
