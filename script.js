34753766a11405be8a97851192d8d3b0ff1653ee
Beth Presten <bethpresten@gmail.com>
2020-10-13 14:26:32 -0400

    uvindex is now populating from the response

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ $(document).ready(function () {
         var city = $("#search-city").val();
         var queryURL = "http://api.openweathermap.org/data/2.5/forecast/?q=" + city + "&appid=e2ce71516635d8291df50c096e9d84fd"
         var apiKey = "e2ce71516635d8291df50c096e9d84fd";
-        // var uvIndexURL = "http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=" + apiKey);
+
+
 
         // console.log(city);
         // console.log(response.city.coord.lat); NOT WORKING; RESPONSE NOT DEFINED?
@@ -32,7 +33,7 @@ $(document).ready(function () {
             // console.log(response.list[0].main.temp);
             // console.log(response.list[0].main.humidity);
             // console.log(response.list[0].wind.speed);
-            console.log(response.list[12].weather[0].icon)
+            // console.log(response.list[12].weather[0].icon)
             // var icon = response.list[12].weather[0].icon;
             JSON.stringify(response.city.name);
             $(".city").attr("style", "<h3>").text(city);
@@ -40,18 +41,37 @@ $(document).ready(function () {
             $("#weather-icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png");
             $(".wind").text("Wind Speed: " + response.list[0].wind.speed + "mph");
             $(".humidity").text("Humidity: " + response.list[0].main.humidity + "%");
-
-            // $(".uv-index").text("UV Index: " + );
             var tempF = (response.list[0].main.temp - 273.15) * 1.8 + 32;
+            var lat = response.city.coord.lat;
+            var lon = response.city.coord.lon;
+
+            // console.log(response.city.coord.lat, response.city.coord.lon);
+
+
+
             // $(".temp").text("Temperature (K) " + response.list[0].main.temp);
             $(".tempF").text("Temperature (F) " + tempF.toFixed(2) + " F");
+
+            var uvIndexURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+
+            $.ajax({
+                url: uvIndexURL,
+                method: "GET",
+            }).then(function (responseUV) {
+                // var UVindex = responseUV[0].value;
+                var UVindex = responseUV.value;
+                console.log(response);
+                console.log(UVindex)
+                // $(".uv-index").text("UV Index: " + UVindex);
+                var uvIndexDiv = $(".uv-index").text("UVIndex " + UVindex);
+
+            })
         })
 
         getCityHistory(city);
 
     })
     // }
-
 
     function getCityHistory(city) {
         var cityHistoryDiv = $("#city-history");
@@ -60,7 +80,17 @@ $(document).ready(function () {
         $("#search-city").val("");
     }
 
+    function fiveDayForecast(data) {
+        queryFiveDay = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+        $.ajax({
+            url: queryFiveDay,
+            method: "GET",
+        }).then(function (data) {
+            console.log(data)
 
+            // var fiveCard = <div class="col-sm-2 card text-white bg-primary mb-3" id="card">
 
+            // </div >
+        });
 
-});
+    });
