c894b38ffd4391bb59b834ef29894d4269191a2e
Beth Presten <bethpresten@gmail.com>
2020-10-14 13:13:15 -0400

    forecast is now hidden until button click and populates upon button click

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 // console.log("This is connected!");
 $(document).ready(function () {
-
+    // global and js variables
     var cityHistory = [];
     var cityHistoryList = $("#city-history");
     var cityInput = $("#city-search");
     var city = $("#search-city").val();
     var weatherImg = $("#weather-icon");
-
     var currentDate = moment().format("HH");
     currentWeather();
 
     function currentWeather() {
 
         $("#search").on("click", function (event) {
-
             var city = $("#search-city").val();
             var queryURL = "http://api.openweathermap.org/data/2.5/forecast/?q=" + city + "&appid=e2ce71516635d8291df50c096e9d84fd"
             var apiKey = "e2ce71516635d8291df50c096e9d84fd";
@@ -50,8 +48,6 @@ $(document).ready(function () {
                 fiveDayForecast(city);
 
                 // console.log(response.city.coord.lat, response.city.coord.lon);
-
-                // $(".temp").text("Temperature (K) " + response.list[0].main.temp);
                 $(".tempF").text("Temperature (F) " + tempF.toFixed(2) + " F");
 
                 var uvIndexURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
@@ -61,16 +57,18 @@ $(document).ready(function () {
                     method: "GET",
                 }).then(function (responseUV) {
                     var UVindex = responseUV.value;
+                    // forcing the uv index to disappear with multiple searches
                     $(".uv-index").empty();
                     var condition = $("<button>");
+                    // adding color to uv index
                     condition.text("UV Index: " + UVindex).attr("class", "btn-warning");
                     $(".uv-index").prepend(condition);
+                    // changed color of the UV index based on the levels
                     if (UVindex < 3) {
                         condition.attr("class", "btn-success");
                     } else if (UVindex > 7) {
                         condition.attr("class", "btn-danger");
                     }
-
                 })
 
                 function fiveDayForecast(city) {
@@ -85,6 +83,7 @@ $(document).ready(function () {
                         $("#five-day").show();
                         $(".card").show();
                         console.log(responseFiveDay);
+                        // First card populated with weather information
                         var dayOneDate = (moment().add(1, 'days').format("L"));
                         console.log(dayOneDate);
                         // console.log(responseFiveDay.list[1].main.temp);
@@ -97,25 +96,25 @@ $(document).ready(function () {
                         // console.log(responseFiveDay.list[1].main.humidity);
                         // console.log(responseFiveDay.list[1].weather[0].icon);
                         $("#five-day-card1").text(dayOneDate, weatherPicOne, tempFone, humidCityOne).append(tempFone, weatherPicOne, humidCityOne);
-
+                        // Second card popoulated with weather information
                         var dayTwoDate = (moment().add(2, 'days').format("L"));
                         var tempFtwo = ("Temp: " + ((response.list[16].main.temp - 273.15) * 1.8 + 32).toFixed(2)) + " F";
                         var weatherPicTwo = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + responseFiveDay.list[16].weather[0].icon + "@2x.png");
                         var humidCityTwo = ("Humidity: " + responseFiveDay.list[16].main.humidity);
                         $("#five-day-card2").text(dayTwoDate, weatherPicTwo, tempFtwo, humidCityTwo).append(tempFtwo, weatherPicTwo, humidCityTwo);
-
+                        // Third card populated with weather information
                         var dayThreeDate = (moment().add(3, 'days').format("L"));
                         var tempFthree = ("Temp: " + ((response.list[23].main.temp - 273.15) * 1.8 + 32).toFixed(2)) + " F";
                         var weatherPicThree = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + responseFiveDay.list[23].weather[0].icon + "@2x.png");
                         var humidCityThree = ("Humidity: " + responseFiveDay.list[23].main.humidity);
                         $("#five-day-card3").text(dayThreeDate, weatherPicThree, tempFthree, humidCityThree).append(tempFthree, weatherPicThree, humidCityThree);
-
+                        // Fourth card populated with weather information
                         var dayFourDate = (moment().add(4, 'days').format("L"));
                         var tempFfour = ("Temp: " + ((response.list[30].main.temp - 273.15) * 1.8 + 32).toFixed(2)) + " F";
                         var weatherPicFour = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + responseFiveDay.list[30].weather[0].icon + "@2x.png");
                         var humidCityFour = ("Humidity: " + responseFiveDay.list[30].main.humidity);
                         $("#five-day-card4").text(dayFourDate, weatherPicFour, tempFfour, humidCityFour).append(tempFfour, weatherPicFour, humidCityFour);
-
+                        // Fifth card populated with weather information
                         var dayFiveDate = (moment().add(5, 'days').format("L"));
                         var tempFfive = ("Temp: " + ((response.list[38].main.temp - 273.15) * 1.8 + 32).toFixed(2)) + " F";
                         var weatherPicFive = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + responseFiveDay.list[38].weather[0].icon + "@2x.png");
@@ -123,20 +122,16 @@ $(document).ready(function () {
                         $("#five-day-card5").text(dayFiveDate, weatherPicFive, tempFfive, humidCityFive).append(tempFfive, weatherPicFive, humidCityFive);
 
                     });
-
                 }
             })
-
             // getCityHistory(city);
-
-            // fiveDayForecast(city);
-
-
         })
 
         // function getCityHistory(city) {
 
         // }
+
+        // Button click on the previously searched cities
         $("#search").on("click", function () {
             city = $("#search-city").val();
             var cityHistoryDiv = $("#city-history");
